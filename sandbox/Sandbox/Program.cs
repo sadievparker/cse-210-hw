ae8d4d922cfe4ba07f3ead9e1cf9812cea6a6d4a
Sadie Parker <sadieparker@byui.edu>
2023-09-16 15:24:07 -0600

    Prep 1

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Console.Write("What is your first name?");
+        string first = Console.ReadLine();
+        Console.Write("What is your last name?");
+        string last = Console.ReadLine();
+        Console.Write($"Your name is {last}, {first} {last}.");
     }
 }
